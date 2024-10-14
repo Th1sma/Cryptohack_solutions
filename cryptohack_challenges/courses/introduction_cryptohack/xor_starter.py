f54f5d1b557e68205661c55f98eabed93cc24182
Th1sma_ <95030586+Th1sma@users.noreply.github.com>
2024-10-14 22:09:20 +0200

    Update xor_starter.py

--- cryptohack_challenges/courses/introduction_cryptohack/xor_starter.py
+++ cryptohack_challenges/courses/introduction_cryptohack/xor_starter.py
@@ -3,10 +3,8 @@ from Crypto.Util.number import bytes_to_long
 string = "label"
 integer = 13
 
-# Convertir la chaîne en bytes
 string_to_bytes = string.encode()
 
-# Appliquer XOR sur chaque byte de la chaîne
 xor_bytes = bytes([b ^ integer for b in string_to_bytes])
 
 print("The flag is:")

a69d988c2e42ab15656955c738080b648e77a756
Thisma17 <mathis.niveau17@outlook.com>
2024-10-14 21:46:01 +0200

    Add new challenge

--- cryptohack_challenges/courses/introduction_cryptohack/xor_starter.py
+++ cryptohack_challenges/courses/introduction_cryptohack/xor_starter.py
@@ -1,7 +1,13 @@
+from Crypto.Util.number import bytes_to_long
+
 string = "label"
 integer = 13
 
-xor_op = "..."
+# Convertir la chaîne en bytes
+string_to_bytes = string.encode()
+
+# Appliquer XOR sur chaque byte de la chaîne
+xor_bytes = bytes([b ^ integer for b in string_to_bytes])
 
 print("The flag is:")
-print()
+print(xor_bytes)
